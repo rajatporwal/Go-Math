a1bc56370c38babfbb560169043eb69cdcba57b4
rajatporwal <rajatporwal1483@gmail.com>
2022-08-01 16:44:17 +0530

    added more apps

--- src/helper/htmlAppsConfig.js
+++ src/helper/htmlAppsConfig.js
@@ -13,6 +13,56 @@ const HTML_APPS_CONFIG = {
         "//jsfiddle.net/rajatporwal/xhL0w3zd/13/embedded/result,js,html,css/dark/",
       note: null,
     },
+    {
+      title: "Card on hover",
+      keywords: "card on hover",
+      id: "html-css/card-on-hover",
+      description: "",
+      list: null,
+      embededUrl:
+        "//jsfiddle.net/rajatporwal/h3685aoc/6/embedded/result,js,html,css/dark/",
+      note: null,
+    },
+    {
+      title: "Loader Circle",
+      keywords: "loader circle",
+      id: "html-css/loader-circle",
+      description: "",
+      list: null,
+      embededUrl:
+        "//jsfiddle.net/rajatporwal/otfp1m2r/2/embedded/result,js,html,css/dark/",
+      note: null,
+    },
+    {
+      title: "Loader Text",
+      keywords: "loader text",
+      id: "html-css/loader-text",
+      description: "",
+      list: null,
+      embededUrl:
+        "//jsfiddle.net/rajatporwal/bx8c5s1t/2/embedded/result,js,html,css/dark/",
+      note: null,
+    },
+    {
+      title: "Loader Circle Horizontal",
+      keywords: "loader circle horizontal",
+      id: "html-css/loader-circle-horizontal",
+      description: "",
+      list: null,
+      embededUrl:
+        "//jsfiddle.net/rajatporwal/d7yxwkqm/embedded/result,js,html,css/dark/",
+      note: null,
+    },
+    {
+      title: "Typewriter Animation",
+      keywords: "typewriter animation",
+      id: "html-css/typewriter-animation",
+      description: "",
+      list: null,
+      embededUrl:
+        "//jsfiddle.net/rajatporwal/yevs4b0w/embedded/result,js,html,css/dark/",
+      note: null,
+    },
   ],
 };
 
